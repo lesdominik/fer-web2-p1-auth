eebb65525f835abd3b9ea1143340dd25c6a913fc
lesdominik <les.dominik@gmail.com>
2024-10-25 17:14:44 +0200

    Add home router

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+var path = require('path');
 
-app.get("/", function (req, res) {
-	res.send("Test hello!");
-});
+app.use(express.static(path.join(__dirname, 'public')));
+
+const homeRouter = require('./routes/home.routes');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/', homeRouter);
 
 app.listen(3030);
