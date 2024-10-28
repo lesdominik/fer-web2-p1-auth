7533d400e90472f6021c5b6d9459104afa5b003d
lesdominik <les.dominik@gmail.com>
2024-10-28 04:33:24 +0100

    Properly handle async functions - add await where needed

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,7 @@
 var model = require('../models/ticketsModel');
 
 exports.getTotalGenereatedTicketsCount = async function (req, res, next) {
-	const count = model.getTotalRowCount();
+	const count = await model.getTotalRowCount();
 
 	res.render('home', {
 		totalCount: count,
