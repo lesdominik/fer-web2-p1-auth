c6b2eaaee8fa2570d46505d9ee63df6ccb0099f4
lesdominik <les.dominik@gmail.com>
2024-10-28 07:55:56 +0100

    Create ticket page

--- controllers/ticketsController.js
+++ controllers/ticketsController.js
@@ -59,16 +59,20 @@ exports.getTicket = async function (req, res, next) {
 	const id = req.params.id;
 	const user = JSON.stringify(req.oidc.user);
 
-	console.log(user);
-
 	try {
 		const ticket = await model.getTicket(id);
 
 		if (!ticket) {
-			return res.send('no ticket with given id.');
+			return res.send('There is no ticket with given id.');
 		}
 
-		return res.send(ticket);
+		return res.render('ticket', {
+			username: user.name,
+			vatin: ticket.vatin,
+			firstName: ticket.firstName,
+			lastName: ticket.lastName,
+			creationTime: ticket.created_at,
+		});
 	} catch (error) {
 		return res.status(500).json({
 			error: 'Error occured while getting ticket with given id',
