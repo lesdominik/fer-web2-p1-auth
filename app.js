75df1625bd057c82eb668219a9df77ea97a445ce
lesdominik <les.dominik@gmail.com>
2024-10-28 03:23:47 +0100

    Revert some changes

--- app.js
+++ app.js
@@ -3,18 +3,12 @@ const app = express();
 var path = require('path');
 const { auth } = require('express-openid-connect');
 require('dotenv').config();
-const fs = require('fs');
-const https = require('https');
-
-const externalUrl = process.env.RENDER_EXTERNAL_URL;
-const port =
-	externalUrl && process.env.PORT ? parseInt(process.env.PORT) : 4080;
 
 const config = {
 	authRequired: false,
 	auth0Logout: true,
 	secret: process.env.SECRET,
-	baseURL: externalUrl || `https://localhost:${port}`,
+	baseURL: 'https://fer-web2-p1-auth.onrender.com',
 	clientID: process.env.CLIENT_ID,
 	issuerBaseURL: 'https://dev-8y8lns3b8lke6ows.us.auth0.com',
 	clientSecret: process.env.CLIENT_SECRET,
@@ -35,23 +29,6 @@ app.set('view engine', 'ejs');
 app.use('/', homeRouter);
 app.use('/tickets', ticketsRouter);
 
-if (externalUrl) {
-	const hostname = '0.0.0.0'; //ne 127.0.0.1
-	app.listen(port, hostname, () => {
-		console.log(
-			`Server locally running at http://${hostname}:${port}/ and from outside on ${externalUrl}`
-		);
-	});
-} else {
-	https
-		.createServer(
-			{
-				key: fs.readFileSync('server.key'),
-				cert: fs.readFileSync('server.cert'),
-			},
-			app
-		)
-		.listen(port, function () {
-			console.log(`Server running at https://localhost:${port}/`);
-		});
-}
+app.listen(3030, () => {
+	console.log('App is running on port 3030');
+});
